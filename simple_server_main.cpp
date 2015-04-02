49ce0aad303333d3d473668b3c21a34b84307119
nenadlazic <nenadlazic13@gmail.com>
2015-04-02 23:44:41 +0200

    simple_server_main.cpp

---
+++ simple_server_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "ServerSocket.h"
+#include "SocketException.h"
+
+int main()
+{
+    std::cout << "Kompajliraaa seee :)!" << std::endl;
+    return 0;
+}
+
