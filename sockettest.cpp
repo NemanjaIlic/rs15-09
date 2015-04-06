90bf1d2d017d04c51a03accddbe7646be1578236
nenadlazic <nenadlazic13@gmail.com>
2015-04-06 03:48:53 +0200

    Create sockettest.cpp

---
+++ sockettest.cpp
@@ -0,0 +1,44 @@
+#include "sockettest.h"
+
+SocketTest::SocketTest(QObject *parent) :
+    QObject(parent)
+{
+}
+
+
+void SocketTest::Connect()
+{
+    //connected
+    socket = new QTcpSocket(this);
+
+    socket->connectToHost("google.com", 80); //random port treba na 80
+                                            //sa nslookup vidis ip adresu ako je potrebno
+
+    //ceka na konekcju 3000ms ili 3sec
+    if(socket->waitForConnected(7000))
+    {
+        qDebug()<< "Uspesno povezivanje sa serverom";
+
+        //send
+        socket->write("Hello server!\r\n\r\n\r\n\r\n"); //pise nesto u socket odn salje serveru, a ovaj treba da mu odgovori
+        socket->waitForBytesWritten(1000); //ceka na odgovor servera 1sekundu
+        socket->waitForReadyRead(3000);
+        qDebug()<< "Reading!"<< socket->bytesAvailable();
+        socket->readAll();
+
+
+        socket->close();
+    }
+    else
+    {
+        qDebug()<< "Neuspelo povezivanje sa serverom";
+    }
+
+
+
+        //sent
+
+        //got
+
+        //closed
+    }
