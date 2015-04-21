890f2054cfb5f41adeca4c1c73629140e68f747f
Graditelj <kuratus@gmail.com>
2015-04-21 20:51:25 +0200

    Create myserver.cpp
    implementacija klase myserver

---
+++ myserver.cpp
@@ -0,0 +1,25 @@
+#include "myserver.h"
+
+MyServer::MyServer(QObject *parent) :
+    QObject(parent)
+{
+    server = new QTcpServer(this);
+    connect(server, SIGNAL(newConnection()), this, SLOT(newConnection()));
+    if(!server->listen(QHostAddress::Any, 1234))
+    {
+        qDebug() << "Server nije pokrenut!";
+    }
+    else
+    {
+        qDebug() << "Server pokrenut!";
+    }
+}
+
+void MyServer::newConnection()
+{
+    QTcpSocket *socket = server->nextPendingConnection();
+                                 socket->write("zdravo klijentelo moja!\r\n"); //pise nesto socketu i treba da salje sledecoj konekciji na redu
+                                 socket->flush();
+                                 socket->waitForBytesWritten(3000);
+                                 socket->close();
+}
